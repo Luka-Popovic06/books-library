27b5a81c752cb6511c66cb8df0b6dbcac6714e97
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-02 22:01:54 +0100

    Implement book update and read status toggle functionality

--- src/Book.jsx
+++ src/Book.jsx
@@ -16,5 +16,63 @@ const Book = (props) => {
     });
     setEditMode(false);
   };
+  return (
+    <>
+      <li className="list-item">
+        {!editMode ? (
+          <>
+            <p className="card-book-name">{title}</p>
+            <p className="card-book-autor">{author}</p>
+            <p className="card-page-number">{pages}</p>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              className="input-mode-edit-name"
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <input
+              type="text"
+              value={editAuthor}
+              onChange={(e) => setEditAuthor(e.target.value)}
+              className="input-mode-edit-autor"
+            />
+            <input
+              type="text"
+              value={editPages}
+              onChange={(e) => setEditPages(e.target.value)}
+              className="input-mode-edit-pagesnumber"
+            />
+          </>
+        )}
+
+        <div className="card-btn">
+          <button
+            type="button"
+            className="btn-read-un"
+            onClick={toggleReadStatus}
+          >
+            {!checked ? "Unread ❌" : "Read ✔️"}
+          </button>
+          <button
+            type="button"
+            className="btn-delete"
+            onClick={() => onDelete(id)}
+          >
+            Delete Book
+          </button>
+          <button
+            type="button"
+            className="btn-edit"
+            onClick={() => (editMode ? finishEditing() : setEditMode(true))}
+          >
+            {!editMode ? "Edit" : "Finish Editing"}
+          </button>
+        </div>
+      </li>
+    </>
+  );
 };
 export default Book;
